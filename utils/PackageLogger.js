69130c9a9cc8d8b7e6b658e086f214b6f4e93198
Nikolay Aleshkovskiy <tehlic@gmail.com>
2023-12-28 15:58:57 +0700

    Squashed 'example/packages/meteor-pagination/package-utils/' changes from ee77dfe..c5e7b13
    c5e7b13 feat: Upgrade types and comments for package-utils
    
    git-subtree-dir: example/packages/meteor-pagination/package-utils
    git-subtree-split: c5e7b13452a165ebe00a28e05a29c4b9ff45917c

--- utils/PackageLogger.js
+++ utils/PackageLogger.js
@@ -2,34 +2,44 @@
 let loggerInstance;
 
 /**
- * Overrides default console .log and .error messages to insert `prefix`
+ * Overrides default console log, warn and error messages to insert `prefix`
  * So any message will be printed like: `${logPrefix}` + `message`
+ * This is to distinguish package logs easily in terminal
  * @type {import('../types').PackageLogger}
- * @return {PackageLoggerInstance}
+ * @return {import('../types').PackageLoggerInstance}
  * */
 export const PackageLogger = ({
   enableLogging = true,
-  logPrefix = 'Package |',
+  logPrefix = 'Package |'
 } = {}) => {
   if (!loggerInstance) {
     loggerInstance = {
       log: function () {
         if (enableLogging) {
-          console.log.apply(this, [logPrefix, ...arguments]);
+          console.log.apply(this, [
+            logPrefix,
+            ...Array.prototype.slice.call(arguments)
+          ]);
         }
       },
 
       error: function () {
-        console.error.apply(this, [logPrefix, ...arguments]);
+        console.error.apply(this, [
+          logPrefix,
+          ...Array.prototype.slice.call(arguments)
+        ]);
       },
 
       warn: function () {
-        console.warn.apply(this, [logPrefix, ...arguments]);
-      },
+        console.warn.apply(this, [
+          logPrefix,
+          ...Array.prototype.slice.call(arguments)
+        ]);
+      }
     };
 
-    loggerInstance.log('PackageLogger inited successfully');
+    loggerInstance.log('PackageLogger initiated successfully');
   }
-  
+
   return loggerInstance;
 };
