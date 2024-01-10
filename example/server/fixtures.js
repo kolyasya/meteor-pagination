f20788a82571b99f52b8c4a1517b463d3835c6ab
Nikolay Aleshkovskiy <tehlic@gmail.com>
2024-01-10 11:11:05 +0700

    fix: Adjust fixtures

--- example/server/fixtures.js
+++ example/server/fixtures.js
@@ -1,22 +1,23 @@
 import { Meteor } from 'meteor/meteor';
-import Jabber from 'jabber';
-import Posts from '/imports/api/posts';
+import { generate } from 'random-words';
+import Posts from '../imports/api/posts';
 
 const insertPost = ({ title, content }) => {
   console.log(`Insert post ${title}`);
-  Posts.insert({title, content, createdAt: new Date()});
-}
+  Posts.insert({ title, content, createdAt: new Date() });
+};
 
 Meteor.startup(() => {
+  // Posts.remove({});
+
   if (Posts.find().count() === 0) {
     console.log('Inserting fixtures to Posts collection...');
-    const jabber = new Jabber();
 
     for (let i = 0; i < 91; i++) {
       insertPost({
-        title: `${jabber.createWord(6, true)} ${jabber.createWord(8)}`,
-        content: jabber.createParagraph(90)
-      })
+        title: `Post #${i}`,
+        content: `${generate(3).join(' ')}`
+      });
     }
 
     console.log('Posts are inserted');
