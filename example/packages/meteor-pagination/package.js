003ef7f31febe1db5d3fd76e7935e2673a58c6af
Nikolay <tehlic@gmail.com>
2024-08-06 11:46:37 +0400

    feat: Version up

--- example/packages/meteor-pagination/package.js
+++ example/packages/meteor-pagination/package.js
@@ -1,17 +1,17 @@
 Package.describe({
   name: 'kolyasya:meteor-pagination',
-  version: '1.0.0-beta.3',
+  version: '1.0.0-beta.4',
   summary: 'Pagination for Meteor. Based on observeChanges and publishCounts',
   git: 'https://github.com/kolyasya/meteor-pagination',
-  documentation: '../../../README.md'
+  documentation: '../../../README.md',
 });
 
-Package.onUse(function (api) {
+Package.onUse(api => {
   api.use([
     'ecmascript@0.16.0',
     'mongo@2.0.0-rc300.0',
     'compat:publish-counts@1.0.0-beta.0',
-    'tmeasday:check-npm-versions@2.0.0-beta.0'
+    'tmeasday:check-npm-versions@2.0.0-beta.0',
   ]);
 
   api.mainModule('server.js', 'server');
